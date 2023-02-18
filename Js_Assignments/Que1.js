0acfca3d3f87ff823dba1ab33a21bc29510eccc5
Ni3Chougale <113974022+Ni3Chougale@users.noreply.github.com>
2023-02-18 12:21:39 +0530

    Modified

--- Js_Assignments/Que1.js
+++ Js_Assignments/Que1.js
@@ -1,5 +1,5 @@
 //Write a JavaScript program to reverse a given string
 function reverseString(str) {
-  return str.split("").rever;
+  return str.split("").reverse("").join('');
 }
 console.log(reverseString("Yash Technology"));
